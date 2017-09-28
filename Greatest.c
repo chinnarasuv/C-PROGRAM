81a355db29737ad2550ed4515d66c432e20c0488
chinnarasuv <30316853+chinnarasuv@users.noreply.github.com>
2017-09-28 10:59:58 +0530

    Create Greatest.c

---
+++ Greatest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf("Enter the no:");
+  scanf("%d%d%d", &a,&b,&c);
+  if(a>b)
+    {
+      printf(" A is Greater\n");
+    }
+  else if(b>c)
+    {
+      printf("B is geater\n");
+    }
+  else
+    {
+      printf(" C is gerater");
+    }
+    
+ }
