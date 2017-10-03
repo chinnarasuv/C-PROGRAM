585504f975e3470a48d5ab5db827dbd885082f9b
chinnarasuv <30316853+chinnarasuv@users.noreply.github.com>
2017-10-03 11:46:50 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float a;
+ printf("Enter the no:");
+ scanf("%f", &a);
+ if(a<0)
+ {
+  printf("Given no is positive");
+ }
+ else if
+ {
+  printf("Given no is negative");
+ }
+ else
+ {
+  printf("Given no is Zero");
+ }
+ getch();
+}
+ 
