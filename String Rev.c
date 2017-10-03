b93ee4cc1481d7544804ce0218f091d96e698f2b
chinnarasuv <30316853+chinnarasuv@users.noreply.github.com>
2017-10-03 12:23:12 +0530

    Create String Rev.c

---
+++ String Rev.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#inculde<string.h>
+#inclde<conio.h>
+void main()
+{
+  char a[10];
+  prntf(" Enter the String:");
+  scanf("%s",&a[10]);
+  strrev(a);
+  printf(" After reverse: ",a);
+  getch();
+}
