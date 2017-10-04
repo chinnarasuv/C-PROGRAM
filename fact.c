4ff076d37f0e5e191b61d9f6d294e83f2fcbf5c6
chinnarasuv <30316853+chinnarasuv@users.noreply.github.com>
2017-10-04 12:22:21 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  int a, b,c=1;
+  printf(" Enter the number: \n");
+  scanf("%d",&a);
+  for(b=0;b<=a;b++)
+  {
+   c=fact*a;
+   printf(" Fcatorial of %d is: %d\n",b, fact);
+ }
+ 
